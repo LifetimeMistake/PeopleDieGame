b4d66212162da155077f5c7af9df209015e1d54c
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2022-09-01 14:40:48 -0700

    augh

--- UnturnedGameMaster/Commands/TestCommand.cs
+++ UnturnedGameMaster/Commands/TestCommand.cs
@@ -1,10 +1,14 @@
 ﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Framework.Translations;
 using SDG.Unturned;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace UnturnedGameMaster.Commands
 {
@@ -24,7 +28,16 @@ namespace UnturnedGameMaster.Commands
 
         public void Execute(IRocketPlayer caller, string[] command)
         {
-            
+            Player player = ((UnturnedPlayer)caller).Player;
+            RaycastHit hit;
+            Ray ray = new Ray(player.look.aim.position, player.look.aim.forward);
+            if (!Physics.Raycast(ray, out hit, 100, RayMasks.LARGE))
+            {
+                UnturnedChat.Say("no luck");
+                return;
+            }
+
+            UnturnedChat.Say($"transform at {hit.transform.position}");
         }
     }
 }
