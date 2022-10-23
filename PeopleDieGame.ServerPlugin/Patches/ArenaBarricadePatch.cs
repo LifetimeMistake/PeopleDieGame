33a35b3e3c3aa8aaf389dc5fc65eb2d3ff796d07
greatlolz <greatlolz.676@gmail.com>
2022-10-23 20:23:39 +0200

    arena building patches

---
+++ PeopleDieGame.ServerPlugin/Patches/ArenaBarricadePatch.cs
@@ -0,0 +1,33 @@
+﻿using PeopleDieGame.ServerPlugin.Autofac;
+using PeopleDieGame.ServerPlugin.Models;
+using PeopleDieGame.ServerPlugin.Services.Managers;
+using HarmonyLib;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PeopleDieGame.ServerPlugin.Patches
+{
+    [HarmonyPatch(typeof(BarricadeManager), "dropBarricade")]
+    public static class ArenaBarricadePatch
+    {
+        public static bool Prefix(ref Vector3 point, ref Transform __result)
+        {
+            ArenaManager arenaManager = ServiceLocator.Instance.LocateService<ArenaManager>();
+            foreach (BossArena arena in arenaManager.GetArenas())
+            {
+                if (Vector3.Distance(arena.ActivationPoint, point) <= arena.DeactivationDistance)
+                {
+                    __result = null;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PeopleDieGame.ServerPlugin/Patches/ArenaStructurePatch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using PeopleDieGame.ServerPlugin.Autofac;
+using PeopleDieGame.ServerPlugin.Models;
+using PeopleDieGame.ServerPlugin.Services.Managers;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PeopleDieGame.ServerPlugin.Patches
+{
+    [HarmonyPatch(typeof(StructureManager), "dropReplicatedStructure")]
+    public static class ArenaStructurePatch
+    {
+        public static bool Prefix(ref Vector3 point, ref bool __result)
+        {
+            ArenaManager arenaManager = ServiceLocator.Instance.LocateService<ArenaManager>();
+            foreach (BossArena arena in arenaManager.GetArenas())
+            {
+                if (Vector3.Distance(arena.ActivationPoint, point) <= arena.DeactivationDistance)
+                {
+                    __result = false;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
